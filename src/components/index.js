99b368f61c2324eba15455cbcd509094bdd49abb
Anurag Tiwari <168359424+AnuragTiwari1508@users.noreply.github.com>
2025-08-17 01:16:06 +0530

    Delete src directory

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navbar } from './Navbar';
-export { default as Main } from './main';
-export { default as Product } from './Products';
-export { default as EnhancedProducts } from './EnhancedProducts';
-export { default as Footer } from './Footer';
